19b5f6377f74f7c7d875f7be7cc23ef7b143f52e
Detemann <lucasdetemann12@gmail.com>
2024-08-16 23:45:01 -0300

    Initial commit

---
+++ api-command/src/main/java/com/sarrus/api_command/ApiCommandApplication.java
@@ -0,0 +1,13 @@
+package com.sarrus.api_command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCommandApplication.class, args);
+	}
+
+}
